0e9f89d23d5247730631359e3afde20b0dfbc314
Austin Johnson <ATJohnson83@Gmail.com>
2017-07-09 15:46:08 -0400

    first push

---
+++ assets/javascript/game.js
@@ -0,0 +1,62 @@
+var composers = ["bach","mozart","beethoven","debussy","stravinsky"];
+var randomComposer = composers[Math.floor(Math.random() * composers.length)];
+var guessComposer = [];
+var remaining = randomComposer.length;	
+	for (var i=0; i<randomComposer.length; i++){
+	guessComposer[i] = "-";
+	}
+	var el = document.getElementById('guessword');
+		el.innerHTML=guessComposer.join("");
+
+
+var wrongGuesses = [];
+// var eleme = document.getElementById('wrong');
+// 		eleme.innerHTML=wrongGuesses;
+
+var guessesLeft = 15;
+var ele = document.getElementById('remaining');
+		ele.innerHTML=guessesLeft;
+
+var wins = 0;
+var elem = document.getElementById('numwins');
+		elem.innerHTML=wins;
+
+document.onkeyup = function(event) {
+	
+	var userGuess = event.key;
+	
+	// el.innerHTML=guessComposer.join(" ");
+	
+
+	for(var j=0; j<randomComposer.length; j++){
+		
+		if(randomComposer[j]===userGuess){
+			guessComposer[j]=userGuess;
+			}
+	}
+	
+	if (userGuess !== randomComposer[j]){
+			wrongGuesses.push(userGuess);
+			guessesLeft --;
+			var wg = document.getElementById('wrong');
+			wg.innerHTML=wrongGuesses;
+		}
+
+		if(guessComposer==randomComposer){
+			alert("Congratulations, You Won!");
+			wins++;
+		}
+
+		if(guessesLeft===0){
+			alert("You lost, study more music...");
+		}
+
+		var el = document.getElementById('guessword');
+		el.innerHTML=guessComposer.join("");
+
+		var ele = document.getElementById('remaining');
+		ele.innerHTML=guessesLeft;
+
+		console.log(randomComposer.length);
+}
+
